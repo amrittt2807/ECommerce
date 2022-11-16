cdbe506120d11703d235fed81b1c564ed36c9d2b
Saubhagya Ashish <followme.saubhagya@gmail.com>
2022-11-16 13:31:46 +0530

    Synced cart drop down with the shopping page

--- src/Components/CartDropDown/CartDropDown.js
+++ src/Components/CartDropDown/CartDropDown.js
@@ -1,12 +1,23 @@
-import React from 'react'
-import Button from '../Button/Button'
-import "./cart-dropdown.styles.scss"
+import React, { useContext } from "react";
+import { ShowCartCard } from "../../Context/ShowCartCard/ShowCartCard";
+import Button from "../Button/Button";
+import CartItem from "../Cart_item/CartItem";
+import "./cart-dropdown.styles.scss";
 
 export const CartDropDown = () => {
+  const { cartItems } = useContext(ShowCartCard);
   return (
-    <div className='cart-dropdown-container' >
-        <div className='cart-item'/>
-        <Button buttonName="GO TO CHECKOUT" style={{fontSize:"small"}} />
+    <div className="cart-dropdown-container">
+      <div className="cart-items">
+        {cartItems.length ? (
+          cartItems.map((cartItem) => (
+            <CartItem key={cartItem.id} cartItem={cartItem} />
+          ))
+        ) : (
+          <span className="empty-message">Your cart is empty</span>
+        )}
+      </div>
+      <Button buttonName="GO TO CHECKOUT" style={{ fontSize: "small" }} />
     </div>
-  )
-}
+  );
+};
--- src/Context/ShowCartCard/ShowCartCard.js
+++ src/Context/ShowCartCard/ShowCartCard.js
@@ -1,14 +1,37 @@
 import { createContext, useState } from "react";
 
+export const addCartItem = (cartItems, productToAdd) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === productToAdd.id
+  );
+
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === productToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
+
 export const ShowCartCard = createContext({
-    
+  showDropDown: false,
+  setShowDropDown: () => {},
+  cartItems: [],
 });
 export const ShowCartCardProvider = ({ children }) => {
   const [showDropDown, setShowDropDown] = useState(false);
-  const value = {showDropDown,setShowDropDown}
+
+  const [cartItems, setCartItems] = useState([]);
+
+  const addItemToCart = (product) =>
+    setCartItems(addCartItem(cartItems, product));
+
+  const value = { showDropDown, setShowDropDown, cartItems, addItemToCart };
+
   return (
-    <ShowCartCard.Provider value={value}>
-      {children}
-    </ShowCartCard.Provider>
+    <ShowCartCard.Provider value={value}>{children}</ShowCartCard.Provider>
   );
 };
